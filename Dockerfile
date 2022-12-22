543610402c4d60ec2e3f139177abae63df400916
Bryan Oliver <boliver@linux.com>
2022-12-22 13:09:24 -0500

    rust echo server

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,32 @@
-# Dockerfile for creating a statically-linked Rust application using docker's
-# multi-stage build feature. This also leverages the docker build cache to avoid
-# re-downloading dependencies if they have not changed.
-FROM docker.io/rust:1.58.1-buster AS production-build
-WORKDIR /usr/src
+# Build image
+FROM rust:alpine as builder
 
-# Create a dummy project and build the app's dependencies.
-# If the Cargo.toml or Cargo.lock files have not changed,
-# we can use the docker build cache and skip these (typically slow) steps.
-RUN rustup target add x86_64-unknown-linux-musl && \
-    cargo new basic-bot
-WORKDIR /usr/src/basic-bot
-COPY Cargo.toml Cargo.lock ./
+RUN apk add --no-cache musl-dev
+
+RUN mkdir /server
+WORKDIR /server
+COPY . .
+
 RUN cargo build --release
 
-# Copy the source and build the application.
-COPY src ./src
-RUN cargo install --target x86_64-unknown-linux-musl --path .
+# Final image
+FROM alpine:latest
 
-# Copy the statically-linked binary into a scratch container.
-FROM scratch
-WORKDIR /
-COPY --from=production-build /usr/local/cargo/bin/basic-bot .
-USER 1000
-CMD ["./basic-bot"]
+ENV USER="app"
+
+# Define user that executes the echo-server
+RUN addgroup -S $USER
+RUN adduser -S -g $USER $USER
+
+RUN mkdir /server
+WORKDIR /server
+
+COPY --from=builder /server/target/release/echo-server /server/echo-server
+RUN chown -R $USER:$USER /server
+
+USER $USER
+
+# Expose default port of echo-server
+EXPOSE 8080
+
+ENTRYPOINT ["/server/echo-server"]
--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,100 @@
-use std::env;
-use anyhow::Result;
-use serenity::{async_trait, http::Http, model::{channel::Message, gateway::Ready, id::{ChannelId}}, prelude::*};
-use tokio::time::{interval, Duration};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-struct Handler;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::header::{HeaderValue, CONTENT_LENGTH};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
+use structopt::StructOpt;
 
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
-}
+#[derive(PartialEq, Debug)]
+struct ParseError {}
 
-struct Responder;
+/// Find acronym meaning.
+#[derive(Debug, StructOpt)]
+#[structopt(name = "args", about = "Provide echo server configuration")]
+struct Cli {
+    /// The acronym to search for
+    #[structopt(short, long)]
+    port: Option<u16>,
 
-#[async_trait]
-impl EventHandler for Responder {
-    /// React to messages
-    /// The actual listening is handled with some Serenity magic.
-    /// Here, we just match on the message content and react .
-    async fn message(&self, ctx: Context, msg: Message) {
-        match msg.content.as_ref() {
-            "!check" => match Updater::react(&msg.channel_id, &ctx).await {
-                Ok(_) => {}
-                Err(err) => println!("{}", err),
-            },
-            "lorem ipsum" => {},
-            _ => {}
-        }
-    }
-    async fn ready(&self, _ctx: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
-}
-
-pub struct Updater {
-    token: String,
-    channel_id: ChannelId,
-}
-
-impl Updater {
-    pub fn updater_new(token: String, channel_id: ChannelId) -> Result<Self> {
-        Ok(
-        Self{
-            token, channel_id
-        })
-    }
-
-    pub fn updater_from_config(token: String, channel_id: ChannelId) -> Result<Self> {
-        Self::updater_new(token, channel_id)
-    }
-
-    // TODO - msg passing
-    async fn react(channel_id: &ChannelId, ctx: &Context) -> Result<Message> {
-        let msgContent = "Hello!";
-        let msg = channel_id.say(&ctx.http, &msgContent).await?;
-        Ok(msg)
-    }
-
-    pub async fn update_loop(&self) -> Result<()> {
-        let http = &Http::new_with_token(&self.token);
-        let mut interval = interval(Duration::from_secs(10));
-
-        // Set to true to enable looping messages to the channel
-        //   TODO - this can go away when we build in a proper debug fn and environment variable
-        let debug_loop_enabled = false;
-        loop {
-            interval.tick().await;
-            if debug_loop_enabled {
-                self.channel_id.say(http, "Hey preview deployement!").await?;
-            }
-        }
-    }
-}
-
-pub async fn try_responder_client_and_updater_from_config(token: String) -> Result<(Client, Updater)> {
-        let responder = Client::builder(&token)
-            .event_handler(Responder)
-            .await
-            .expect("Err creating client");
-
-        let channel_id = env::var("CHANNEL_ID").expect("Expected a channel_id in the environment");
-        let channel_u64 = channel_id.parse::<u64>().unwrap();
-        let channel_id = ChannelId(channel_u64);
-        let updater = Updater::updater_from_config(token, channel_id)?;
-        Ok((responder, updater))
+    /// Context to search in
+    #[structopt(short, long)]
+    body: Option<String>,
 }
 
 #[tokio::main]
 async fn main() {
+    let args = Cli::from_args();
+    let port = args.port.unwrap_or(8080);
+    let address = SocketAddr::from(([0, 0, 0, 0], port));
+    let server = Server::bind(&address).serve(make_service_fn(|_server| async {
+        Ok::<_, Infallible>(service_fn(handle_request))
+    }));
 
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let (mut responder, updater) = try_responder_client_and_updater_from_config(token).await.expect("Create bot failed.");
+    // Allow server to be killed.
+    let server = server.with_graceful_shutdown(async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to add signal handler")
+    });
 
-    tokio::select! {
-        _ = updater.update_loop() => {
-            println!("The updater stopped unexpectedly")
-        }
-        _ = responder.start() => {
-            println!("The responder stopped unexpectedly")
-        }
-    };
+    println!("Echo server listening on port {}", port);
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
 }
 
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+    let echo_headers = response.headers_mut();
+    let headers = req.headers();
 
+    // Echo HTTP headers
+    headers.iter().for_each(|(name, value)| {
+        echo_headers.insert(name, value.clone());
+    });
+
+    // Handle each HTTP verb
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            let args = Cli::from_args();
+            let body = args.body.unwrap_or_else(|| String::from(""));
+            let content_length = HeaderValue::from_str(
+                &body.as_bytes().len().to_string()
+            ).unwrap_or_else(|_| HeaderValue::from_static("0"));
+
+            echo_headers.insert(CONTENT_LENGTH, content_length);
+            *response.body_mut() = Body::from(body);
+        }
+        (&Method::POST, "/") => {
+            *response.body_mut() = req.into_body();
+        }
+        (&Method::PUT, "/") => {
+            *response.body_mut() = req.into_body();
+        }
+        (&Method::PATCH, "/") => {
+            *response.body_mut() = req.into_body();
+        }
+        (&Method::OPTIONS, "/") => {
+            *response.status_mut() = StatusCode::OK;
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Ok(response)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get_body() {
+        let resp = tokio_test::block_on(handle_request(Request::new(Body::from("hello world"))));
+
+        assert_eq!(resp.unwrap().status(), StatusCode::OK);
+    }
+}
